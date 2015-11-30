294ca21e77519c4b5a0b43aa6e8564f88665c4fc
kshen3778 <kshen3778@gmail.com>
2015-11-30 09:43:36 -0500

    grkhfjkDhjdsfjhsdfjhdsfjhsdfhjsdfjh

---
+++ FullFileIO.java
@@ -0,0 +1,15 @@
+// The "FullFileIO" class.
+import java.awt.*;
+import hsa.Console;
+
+public class FullFileIO
+{
+    static Console c;           // The output console
+    
+    public static void main (String[] args)
+    {
+	c = new Console ();
+	
+	// Place your program here.  'c' is the output console
+    } // main method
+} // FullFileIO class
