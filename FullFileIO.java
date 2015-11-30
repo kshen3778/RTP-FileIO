75d88ed0722a8910e12edc5bc2f73b3591d2dc07
TheVirtualMachine <vincentdmacri@gmail.com>
2015-11-30 09:59:42 -0500

    Added constructor.

--- FullFileIO.java
+++ FullFileIO.java
@@ -2,15 +2,26 @@
 import java.awt.*;
 import hsa.Console;
 
-/** @author Vincent Macri and Kevin Shen
+/** @author Vincent Macri
+	@author Kevin Shen
+	
+	This program demonstrates full file IO. The user can store a number.
+
+*/
 public class FullFileIO
 {
     static Console c;           // The output console
     
-    public static void main (String[] args)
-    {
-	c = new Console ();
+    public static void main (String[] args) {
+		FullFileIO f = new FullFileIO();
+    }
 	
-	// Place your program here.  'c' is the output console
-    } // main method
-} // FullFileIO class
+	/** @author Vincent Macri
+		Creates a new instance of the FullFileIO class.
+		Creates a new Console with a window name.
+	*/
+	public FullFileIO(){
+		c = new Console("Full File IO");
+	}
+	
+} /* FullFileIO class */
