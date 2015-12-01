50455158b87714c3a7366cbc1368581f0e0d2e4a
TheVirtualMachine <vincentdmacri@gmail.com>
2015-12-01 09:15:14 -0500

    Start of Class 1/12/2015
    Made Console not static. Formatting.

--- FullFileIO.java
+++ FullFileIO.java
@@ -1,27 +1,29 @@
-// The "FullFileIO" class.
+// The "FullFileIO" clasasfs.
 import java.awt.*;
 import hsa.Console;
 
 /** @author Vincent Macri
 	@author Kevin Shen
-	
+
 	This program demonstrates full file IO. The user can store a number.
 
 */
 public class FullFileIO
 {
-    static Console c;           // The output console
-    
-    public static void main (String[] args) {
-		FullFileIO f = new FullFileIO();
+    Console c;           // The output console
+
+    public static void main (String[] args)
+    {
+	FullFileIO f = new FullFileIO ();
     }
-	
-	/** @author Vincent Macri
-		Creates a new instance of the FullFileIO class.
-		Creates a new Console with a window name.
-	*/
-	public FullFileIO(){
-		c = new Console("Full File IO");
-	}
-	
+
+
+    /** @author Vincent Macri
+	    Creates a new instance of the FullFileIO class.
+	    Creates a new Console with a window name.
+    */
+    public FullFileIO ()
+    {
+	c = new Console ("Full File IO");
+    }
 } /* FullFileIO class */
